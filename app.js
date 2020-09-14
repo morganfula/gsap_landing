8dbf45f68ad7d46311ca727281f137bdb71a8259
morganfula <morgan.fula@me.com>
2020-09-14 13:41:56 +0200

    finished

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
+const sliderTexture = document.querySelector('.slider-texture');
 const logo = document.querySelector('#logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
@@ -9,6 +10,7 @@ const tl = new TimelineMax();
 tl.fromTo(hero, 1.5, { height: "0%" }, { height: '80%', ease: Power2.easeInOut })
   .fromTo(hero, 1.3, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
   .fromTo(slider, 1.3, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1.3")
+  .fromTo(sliderTexture, 1.3, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1.3")
   .fromTo(logo, 0.8, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.8")
   .fromTo(hamburger, 0.8, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.8")
   .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
