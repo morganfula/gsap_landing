df88a9bf54f63e6f1956b8710e83db55facc27c2
morganfula <morgan.fula@me.com>
2020-09-14 12:57:54 +0200

    v1

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ const headline = document.querySelector('.headline');
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero, 1, { height: "0%" }, { height: '80%', ease: Power2.easeInOut })
-  .fromTo(hero, 1.2, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
+tl.fromTo(hero, 1.5, { height: "0%" }, { height: '80%', ease: Power2.easeInOut })
+  .fromTo(hero, 1.3, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
+  .fromTo(slider, 1.3, { x: "-100%" }, { x: '0%', ease: Power2.easeInOut }, "-=1.3")
+  .fromTo(logo, 0.8, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.8")
+  .fromTo(hamburger, 0.8, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.8")
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
